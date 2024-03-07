d611e68475cfaba5b324fff70d2fb2dcaac43eee
Garrett Bischof <bischofgarrett@gmail.com>
2024-03-06 21:14:57 -0500

    Animated Splash is kind of working

--- lib/splash/pages/splash_page.dart
+++ lib/splash/pages/splash_page.dart
@@ -1,12 +1,28 @@
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:thunder/settings/pages/settings_page.dart';
+import 'package:lottie/lottie.dart';
+import 'package:thunder/thunder/thunder.dart';
+
+PageController thunderPageController = PageController(initialPage: 1);
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedSplashScreen(splash: Column(children: [Image.asset('assets/logo.png'), const Text("MoversMarket")]), nextScreen: SettingsPage());
+    return AnimatedSplashScreen(
+        splash: SizedBox(
+          height: 400,
+          child: OverflowBox(
+            minHeight: 400,
+            maxHeight: 400,
+            child: Lottie.asset(
+              'assets/animation1.json',
+              repeat: true,
+            ),
+          ),
+        ),
+        nextScreen: Thunder(pageController: thunderPageController),
+        duration: 4000);
   }
 }
