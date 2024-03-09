a0c0dd8430ffdc33f581fe8d9e19a5dcb4c58443
Garrett Bischof <bischofgarrett@gmail.com>
2024-03-08 19:04:19 -0500

    Got splash screen transition working finally

--- lib/splash/pages/splash_page.dart
+++ lib/splash/pages/splash_page.dart
@@ -1,18 +1,38 @@
-import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:lottie/lottie.dart';
-import 'package:thunder/thunder/thunder.dart';
+import 'dart:async';
 
 PageController thunderPageController = PageController(initialPage: 0);
 
-class SplashScreen extends StatelessWidget {
-  const SplashScreen({Key? key}) : super(key: key);
+class SplashScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return SplashState();
+  }
+}
+
+class SplashState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    startTime();
+  }
+
+  startTime() async {
+    var duration = new Duration(seconds: 6);
+    return new Timer(duration, route);
+  }
+
+  route() {
+    context.goNamed("home");
+  }
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedSplashScreen(
-        splash: SizedBox(
+    return Container(
+        decoration: new BoxDecoration(color: Colors.red),
+        child: SizedBox(
           height: 400,
           child: OverflowBox(
             minHeight: 400,
@@ -22,40 +42,6 @@ class SplashScreen extends StatelessWidget {
               repeat: true,
             ),
           ),
-        ),
-        nextScreen: const NextScreen(),
-        duration: 4000);
-  }
-}
-
-/*
-class NextScreen extends StatelessWidget {
-  const NextScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    context.go('/home');
-    return const Text("");
-  }
-}
-*/
-
-class NextScreen extends StatefulWidget {
-  const NextScreen({super.key});
-
-  @override
-  State<NextScreen> createState() => _NextScreen();
-}
-
-class _NextScreen extends State<NextScreen> {
-  @override
-  void initState() {
-    context.go('/home');
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Container();
+        ));
   }
 }
